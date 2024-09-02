6a7a4f023c9a0e152b7195e6d06f6004476a930f
volodimich <burittosforme@gmail.com>
2024-09-02 15:22:08 +0700

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,6 @@
+number_of_homework = 12
+hours_to_homework = 1.5
+CourseName = 'Python'
+TimePerTask = hours_to_homework / number_of_homework
+zpt = ','
+print('Курс:' , CourseName , zpt , 'всего задач:' , number_of_homework, zpt, 'затрачено часов:' , hours_to_homework , zpt , 'среднее время выполнения:' , TimePerTask     )
